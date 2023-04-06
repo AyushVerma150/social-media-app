f199352d4850d9c9bfa86e6bf5a617fee975c55d
Ayush <=>
2023-04-07 02:34:28 +0530

    Minor Fixes and Improvements
    Signed-off-by: Ayush <=>

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserController.java
@@ -1,8 +1,10 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -15,7 +17,25 @@ public class UserController {
     }
 
     @GetMapping(path = "/users")
-    public List<User> findAll(){
-        return this.userDaoService.findAll();
+    public List<User> findAll() {
+        return userDaoService.findAll();
+    }
+
+
+    @GetMapping(path = "/users/{id}")
+    public User findById(@PathVariable Integer id) {
+        User user = userDaoService.findById(id);
+        if (user == null) {
+            throw new UserNotFoundException("id : " + id);
+        }
+        return user;
+    }
+
+    // Create a New user
+    @PostMapping("users")
+    public ResponseEntity<User> create(@RequestBody User user) {
+        User savedUser = userDaoService.create(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+        return ResponseEntity.created(location).build();
     }
 }
--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -1,12 +1,12 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class UserDaoService {
@@ -15,7 +15,7 @@ public class UserDaoService {
     // FindOne
 
     // Static List for now
-    private static List<User> users = new ArrayList<>();
+    private static final List<User> users = new ArrayList<>();
 
     static {
         users.add(new User(1, "Ayush Verma", LocalDate.now().minusYears(30)));
@@ -24,7 +24,17 @@ public class UserDaoService {
         users.add(new User(4, "Aman", LocalDate.now().minusYears(33)));
     }
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return users;
+    }
+
+    public User findById(Integer id) {
+        return users.stream().filter(user -> Objects.equals(user.getId(), id)).findFirst().orElse(null);
+    }
+
+    public User create(User user) {
+        user.setId(users.size() + 1);
+        users.add(user);
+        return user;
     }
 }
